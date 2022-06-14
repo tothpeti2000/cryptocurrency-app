cbae820b1e4ab165d600e80c9a6d928e7cd89c45
tothpeti2000 <60776365+tothpeti2000@users.noreply.github.com>
2022-06-14 14:50:04 +0200

    InputFieldProps modified to use specific HTML types

--- src/interfaces/props.ts
+++ src/interfaces/props.ts
@@ -1,9 +1,10 @@
-import { Control } from "react-hook-form";
+import { HTMLInputTypeAttribute } from "react";
+import { Control, Path } from "react-hook-form";
 import { UserCredentials } from "./user";
 
 export interface InputFieldProps {
-  name: "userName" | "password";
+  name: Path<UserCredentials>;
   label: string;
-  type: string;
+  type: HTMLInputTypeAttribute;
   control: Control<UserCredentials>;
 }
