b85eec5bbcbec7a903089939dbaa6270083ba06a
tothpeti2000 <60776365+tothpeti2000@users.noreply.github.com>
2022-06-14 15:01:20 +0200

    Pages with routing added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
-import LoginForm from "./components/login/LoginForm";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import CurrenciesPage from "./pages/CurrenciesPage";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
-  return <LoginForm />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="currencies" element={<CurrenciesPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/pages/CurrenciesPage.tsx
@@ -0,0 +1,5 @@
+const CurrenciesPage = () => {
+  return <h1>Hello World!</h1>;
+};
+
+export default CurrenciesPage;
