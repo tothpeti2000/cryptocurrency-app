6d9dce94c4dadfc6a2631931fabd3befde046d5a
tothpeti2000 <60776365+tothpeti2000@users.noreply.github.com>
2022-06-14 17:20:28 +0200

    CurrenciesPage renamed to Dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import CurrenciesPage from "./pages/Dashboard";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
 import LoginPage from "./pages/LoginPage";
 import NotFound from "./pages/NotFound";
 
@@ -8,7 +8,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<LoginPage />} />
-        <Route path="dashboard" element={<CurrenciesPage />} />
+        <Route path="dashboard" element={<Dashboard />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
--- src/pages/CurrenciesPage.tsx
+++
@@ -1,5 +0,0 @@
-const CurrenciesPage = () => {
-  return <h1>Hello World!</h1>;
-};
-
-export default CurrenciesPage;
