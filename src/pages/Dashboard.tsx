eed40cd7ca6d1b5b1ee44c6166132cd96e4f95b1
tothpeti2000 <60776365+tothpeti2000@users.noreply.github.com>
2022-06-14 22:14:32 +0200

    LoginPage layout modified

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,4 +1,4 @@
-import Header from "../components/header/Header";
+import Header from "../components/Header";
 
 const Dashboard = () => {
   return <Header userName="User" />;
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,7 +1,23 @@
+import { Box, Grid } from "@mui/material";
 import LoginForm from "../components/login/LoginForm";
+import Title from "../components/Title";
 
 const LoginPage = () => {
-  return <LoginForm />;
+  return (
+    <Grid
+      className="bg-radial"
+      container
+      direction={"column"}
+      justifyContent={"center"}
+      alignItems={"center"}
+      height={"100vh"}
+    >
+      <Box>
+        <Title />
+        <LoginForm />
+      </Box>
+    </Grid>
+  );
 };
 
 export default LoginPage;
