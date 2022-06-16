965cf1dc7fdbb5b1c119d129a92cc392b4689d07
tothpeti2000 <60776365+tothpeti2000@users.noreply.github.com>
2022-06-16 13:27:50 +0200

    Revert "/dashboard route made protected"
    This reverts commit 9f9e829012df3ef128ed683cd667c326862e281b.

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,22 +1,10 @@
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
 import AddCurrencyDialog from "../components/AddCurrencyDialog";
 import Chart from "../components/Chart";
 import CurrencyList from "../components/currencyList/CurrencyList";
 import Header from "../components/Header";
 import CurrencyTabs from "../components/tabs/CurrencyTabs";
-import { useUserContext } from "../context/UserContext";
 
 const Dashboard = () => {
-  const { user } = useUserContext();
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    if (user === undefined) {
-      navigate("/");
-    }
-  });
-
   return <Header />;
 };
 
